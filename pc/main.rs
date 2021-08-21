db7528afbc1382a3eee2b512878cbe87ecc9a961
Chris Lewis <cl_myles@hotmail.com>
2021-08-21 14:46:49 +1000

    Add room and coloured sprite to pc

--- pc/main.rs
+++ pc/main.rs
@@ -1,17 +1,26 @@
 use raylib::prelude::*;
 
+const WINDOW_WIDTH: i32 = 640;
+const WINDOW_HEIGHT: i32 = 352;
+
 fn main() {
     let (mut rl, thread) = raylib::init()
-        .size(640, 352)
+        .size(WINDOW_WIDTH, WINDOW_HEIGHT)
         .title("Hello, World")
         .build();
 
-    let texture = rl.load_texture(&thread, "sprite.png").expect("");
+    // Put the window in the corner (useful when watching)
+    let monitor_width = raylib::core::window::get_monitor_width(0);
+    rl.set_window_position(monitor_width - WINDOW_WIDTH, 40);
+
+    let room = rl.load_texture(&thread, "room.png").expect("Failed to load room.png");
+    let sprite = rl.load_texture(&thread, "sprite.png").expect("Failed to load sprite.png");
 
     while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
+        let mut screen = rl.begin_drawing(&thread);
 
-        d.clear_background(Color::BLACK);
-        d.draw_texture_ex(&texture, Vector2::new(300.0, 150.0), 0.0, 3.0, Color::WHITE);
+        screen.clear_background(Color::BLACK);
+        screen.draw_texture_ex(&room, Vector2::new(0.0, 0.0), 0.0, 4.0, Color::WHITE);
+        screen.draw_texture_ex(&sprite, Vector2::new(300.0, 150.0), 0.0, 4.0, Color::WHITE);
     }
 }
