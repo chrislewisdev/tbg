2181c89ab9fe2865131e1dde69b1679bc44d5d1b
Chris Lewis <cl_myles@hotmail.com>
2021-08-29 11:28:32 +1000

    Add basic movement (no collision) to pc

--- pc/main.rs
+++ pc/main.rs
@@ -1,7 +1,90 @@
 use raylib::prelude::*;
 
-const WINDOW_WIDTH: i32 = 640;
-const WINDOW_HEIGHT: i32 = 352;
+const TILE_SIZE: i32 = 8;
+const SCALE_FACTOR: i32 = 4;
+
+const SCREEN_WIDTH_CELLS: i32 = 20;
+const SCREEN_HEIGHT_CELLS: i32 = 11;
+
+const WINDOW_WIDTH: i32 = SCREEN_WIDTH_CELLS * TILE_SIZE * SCALE_FACTOR;
+const WINDOW_HEIGHT: i32 = SCREEN_HEIGHT_CELLS * TILE_SIZE * SCALE_FACTOR;
+
+struct Resources {
+    room: Texture2D,
+    player_sprite: Texture2D,
+}
+
+trait Unit {
+    fn get_x(&self) -> i32;
+    fn set_x(&mut self, x: i32);
+    fn get_y(&self) -> i32;
+    fn set_y(&mut self, y: i32);
+}
+
+struct Player {
+    x: i32,
+    y: i32,
+    get_cmd: fn (&RaylibHandle) -> Option<Cmd>,
+}
+
+impl Unit for Player {
+    fn get_x(&self) -> i32 { self.x }
+    fn set_x(&mut self, x: i32) { self.x = x }
+    fn get_y(&self) -> i32 { self.y }
+    fn set_y(&mut self, y: i32) { self.y = y}
+}
+
+struct GlobalState {
+    player: Player,
+}
+
+type Cmd = fn (&mut dyn Unit);
+
+fn cmd_move_left(unit: &mut dyn Unit) {
+    unit.set_x(unit.get_x() - 1);
+}
+fn cmd_move_right(unit: &mut dyn Unit) {
+    unit.set_x(unit.get_x() + 1);
+}
+fn cmd_move_up(unit: &mut dyn Unit) {
+    unit.set_y(unit.get_y() - 1);
+}
+fn cmd_move_down(unit: &mut dyn Unit) {
+    unit.set_y(unit.get_y() + 1);
+}
+
+fn player_control(rl: &RaylibHandle) -> Option<Cmd> {
+    if rl.is_key_down(KeyboardKey::KEY_LEFT) {
+        return Some(cmd_move_left);
+    } else if rl.is_key_down(KeyboardKey::KEY_RIGHT) {
+        return Some(cmd_move_right);
+    } else if rl.is_key_down(KeyboardKey::KEY_UP) {
+        return Some(cmd_move_up);
+    } else if rl.is_key_down(KeyboardKey::KEY_DOWN) {
+        return Some(cmd_move_down);
+    }
+
+    return None;
+}
+
+fn update_game(rl: &RaylibHandle, global_state: &mut GlobalState) {
+    let get_cmd = global_state.player.get_cmd;
+
+    if let Some(cmd) = get_cmd(rl) {
+        cmd(&mut global_state.player);
+    }
+}
+
+fn draw_game(rl: &mut RaylibHandle, thread: &RaylibThread, resources: &Resources, global_state: &GlobalState) {
+    let mut screen = rl.begin_drawing(&thread);
+
+    screen.clear_background(Color::BLACK);
+    screen.draw_texture_ex(&resources.room, Vector2::new(0.0, 0.0), 0.0, SCALE_FACTOR as f32, Color::WHITE);
+
+    let x = global_state.player.x * TILE_SIZE * SCALE_FACTOR;
+    let y = global_state.player.y * TILE_SIZE * SCALE_FACTOR;
+    screen.draw_texture_ex(&resources.player_sprite, Vector2::new(x as f32, y as f32), 0.0, SCALE_FACTOR as f32, Color::WHITE);
+}
 
 fn main() {
     let (mut rl, thread) = raylib::init()
@@ -9,18 +92,20 @@ fn main() {
         .title("Hello, World")
         .build();
 
+    rl.set_target_fps(60);
+
     // Put the window in the corner (useful when watching)
     let monitor_width = raylib::core::window::get_monitor_width(0);
     rl.set_window_position(monitor_width - WINDOW_WIDTH, 40);
 
     let room = rl.load_texture(&thread, "room.png").expect("Failed to load room.png");
     let sprite = rl.load_texture(&thread, "sprite.png").expect("Failed to load sprite.png");
+    let resources = Resources { room: room, player_sprite: sprite };
+
+    let mut global_state = GlobalState { player: Player { x: 3, y: 3, get_cmd: player_control } };
 
     while !rl.window_should_close() {
-        let mut screen = rl.begin_drawing(&thread);
-
-        screen.clear_background(Color::BLACK);
-        screen.draw_texture_ex(&room, Vector2::new(0.0, 0.0), 0.0, 4.0, Color::WHITE);
-        screen.draw_texture_ex(&sprite, Vector2::new(300.0, 150.0), 0.0, 4.0, Color::WHITE);
+        update_game(&rl, &mut global_state);
+        draw_game(&mut rl, &thread, &resources, &global_state);
     }
 }
