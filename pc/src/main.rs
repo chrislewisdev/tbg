55585982ab9d470f541de4098451d4d3463929ab
Chris Lewis <cl_myles@hotmail.com>
2021-09-06 19:08:17 +1000

    Map drawing and collision for pc

--- pc/src/main.rs
+++ pc/src/main.rs
@@ -14,8 +14,8 @@ const WINDOW_WIDTH: i32 = SCREEN_WIDTH_CELLS * TILE_SIZE * SCALE_FACTOR;
 const WINDOW_HEIGHT: i32 = SCREEN_HEIGHT_CELLS * TILE_SIZE * SCALE_FACTOR;
 
 struct Resources {
-    room: Texture2D,
     player_sprite: Texture2D,
+    tileset: Texture2D,
 }
 
 struct InputState {
@@ -35,7 +35,7 @@ trait Unit {
 struct Player {
     cell: Point,
     pixel: Point,
-    get_cmd: fn (&dyn Unit, &InputState) -> Option<Box<dyn Cmd>>,
+    get_cmd: fn (&dyn Unit, &InputState, &GlobalState) -> Option<Box<dyn Cmd>>,
 }
 
 impl Unit for Player {
@@ -66,8 +66,8 @@ impl Cmd for MoveCmd {
         let target_pixel = self.target * TILE_SIZE;
         let unit_pixel = unit.get_pixel();
         let move_pixels = Point {
-            x: i32::signum(target_pixel.x - unit_pixel.x),
-            y: i32::signum(target_pixel.y - unit_pixel.y),
+            x: i32::signum(target_pixel.x - unit_pixel.x) * 2,
+            y: i32::signum(target_pixel.y - unit_pixel.y) * 2,
         };
         
         unit.move_pixels(move_pixels);
@@ -84,15 +84,18 @@ fn is_empty(map: &MapData, x: usize, y: usize) -> bool {
     return map[y][x] == 1;
 }
 
-fn player_control(unit: &dyn Unit, input_state: &InputState) -> Option<Box<dyn Cmd>> {
-    // TODO: Add collision checking
-    if input_state.is_left_pressed {
+fn player_control(unit: &dyn Unit, input_state: &InputState, global_state: &GlobalState) -> Option<Box<dyn Cmd>> {
+    let cell = unit.get_cell();
+    let x = cell.x as usize;
+    let y = cell.y as usize;
+
+    if input_state.is_left_pressed && is_empty(&global_state.map, x - 1, y) {
         return Some(Box::new(MoveCmd { target: unit.get_cell() + point::LEFT }));
-    } else if input_state.is_right_pressed {
+    } else if input_state.is_right_pressed && is_empty(&global_state.map, x + 1, y){
         return Some(Box::new(MoveCmd { target: unit.get_cell() + point::RIGHT }));
-    } else if input_state.is_up_pressed {
+    } else if input_state.is_up_pressed && is_empty(&global_state.map, x, y - 1) {
         return Some(Box::new(MoveCmd { target: unit.get_cell() + point::UP }));
-    } else if input_state.is_down_pressed {
+    } else if input_state.is_down_pressed && is_empty(&global_state.map, x, y + 1) {
         return Some(Box::new(MoveCmd { target: unit.get_cell() + point::DOWN }));
     }
 
@@ -113,7 +116,7 @@ impl ProgramState for ExploreState {
         // Get a new cmd if we have none or the current is done.
         if self.cmd.as_ref().map_or(true, |cmd| cmd.is_done(&global_state.player)) {
             let get_cmd = global_state.player.get_cmd;
-            self.cmd = get_cmd(&global_state.player, input_state);
+            self.cmd = get_cmd(&global_state.player, input_state, global_state);
         }
 
         match &mut self.cmd {
@@ -128,16 +131,18 @@ impl ProgramState for ExploreState {
         let mut screen = rl.begin_drawing(&thread);
     
         screen.clear_background(Color::BLACK);
-        screen.draw_texture_ex(&resources.room, Vector2::new(0.0, 0.0), 0.0, SCALE_FACTOR as f32, Color::WHITE);
-    
+        // TODO: Render map data out
+        // screen.draw_texture_ex(&resources.room, Vector2::new(0.0, 0.0), 0.0, SCALE_FACTOR as f32, Color::WHITE);
+        draw_map(&mut screen, &global_state.map, &resources.tileset);
+
         let x = global_state.player.pixel.x * SCALE_FACTOR;
         let y = global_state.player.pixel.y * SCALE_FACTOR;
         screen.draw_texture_ex(&resources.player_sprite, Vector2::new(x as f32, y as f32), 0.0, SCALE_FACTOR as f32, Color::WHITE);
     }
 }
 
-fn load_map() -> Vec<Vec<i32>> {
-    let mut output: Vec<Vec<i32>> = vec![];
+fn load_map() -> MapData {
+    let mut output: MapData = vec![];
     let csv = fs::read_to_string("assets/map.csv").expect("Failed to load map.csv");
     let mut reader = csv::ReaderBuilder::new().has_headers(false).from_reader(csv.as_bytes());
 
@@ -147,6 +152,34 @@ fn load_map() -> Vec<Vec<i32>> {
     }
 
     return output;
+}
+
+fn get_tile_rect(tileset: &Texture2D, index: i32) -> Rectangle {
+    let tiles_width = tileset.width / TILE_SIZE;
+    let x = index % tiles_width;
+    let y = index / tiles_width;
+
+    Rectangle { x: (x * TILE_SIZE) as f32, y: (y * TILE_SIZE) as f32, width: TILE_SIZE as f32 , height: TILE_SIZE as f32 }
+}
+
+fn draw_map(screen: &mut RaylibDrawHandle, map: &MapData, tileset: &Texture2D) {
+    let mut y = 0;
+
+    for row in map {
+        let mut x = 0;
+        for tile in row {
+            screen.draw_texture_pro(
+                tileset,
+                get_tile_rect(&tileset, *tile),
+                Rectangle { x: (x * TILE_SIZE * SCALE_FACTOR) as f32, y: (y * TILE_SIZE * SCALE_FACTOR) as f32, width: (TILE_SIZE * SCALE_FACTOR) as f32, height: (TILE_SIZE * SCALE_FACTOR) as f32 },
+                Vector2 { x: 0.0, y: 0.0 },
+                0.0,
+                Color::WHITE
+            );
+            x += 1;
+        }
+        y += 1;
+    }
 }
 
 fn main() {
@@ -161,9 +194,9 @@ fn main() {
     let monitor_width = raylib::core::window::get_monitor_width(0);
     rl.set_window_position(monitor_width - WINDOW_WIDTH, 40);
 
-    let room = rl.load_texture(&thread, "assets/room.png").expect("Failed to load room.png");
     let sprite = rl.load_texture(&thread, "assets/sprite.png").expect("Failed to load sprite.png");
-    let resources = Resources { room: room, player_sprite: sprite };
+    let tileset = rl.load_texture(&thread, "assets/tileset.png").expect("Failed to load tileset.png");
+    let resources = Resources { player_sprite: sprite, tileset };
 
     let map = load_map();
 
